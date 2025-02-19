ec5ba2255a40aa433f903d3170275d48d6dad758
jverdejo <jverdejo@eqtic.net>
2025-02-19 17:06:12 +0100

    singleton

--- singleton/js/singleton.js
+++ singleton/js/singleton.js
@@ -5,23 +5,27 @@ class Singleton {
     }
 
     constructor() {
-        
+        //genera un random para cada instancia
         this.random = Math.random();
-
+        // evalua si existe con el método instance
         if (Singleton.instance) {
             return Singleton.instance;
         }
-
+        //relaciona el instance con la propia instancia
         Singleton.instance = this;
     }
-
 }
 
 let singleton = new Singleton();
 let singleton2 = new Singleton();
+const singleton3 = Singleton.getInstance();
+
 
 console.log(singleton.random);
 console.log(singleton2.random);
+console.log(singleton3 ? singleton3 : "empty");
+
+console.log(singleton === singleton2);
 
 // EJEMPLO práctico --------------------------------------
 
@@ -57,7 +61,7 @@ class WeekDays{
 }
 
 const weekDays = new WeekDays("en");
-const weekDays2 = new WeekDays();
+const weekDays2 = new WeekDays("es");
 console.log(weekDays.getDays());
 console.log(weekDays2.getDays());
 
